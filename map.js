b4450fc33704ad2912ef7f02a7e70768d72f4bf8
Munsur Rahman <munsurinfo@gmail.com>
2023-05-09 00:06:36 +0600

    28
    28

---
+++ map.js
@@ -0,0 +1,13 @@
+var myMap = new Map();
+
+myMap.set("key1","Bangladesh");
+myMap.set("key2","Bhutan");
+myMap.set("key3","Maldip");
+myMap.set("key4","India");
+myMap.set("key5","Pakisthan");
+myMap.set("key6","Srilanka");
+myMap.set("key7","America");
+myMap.set("key8","Japan");
+myMap.set("key9","Nepal");
+
+console.log(myMap.keys());
---
+++ simpleArray.js
@@ -0,0 +1,8 @@
+var myArr = ["A","B","C","D","E"];
+
+var myArray = new Array("A","B","C","D","E");
+
+for (let item of myArray){
+    console.log(item)
+}
+
